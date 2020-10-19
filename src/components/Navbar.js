63539239777b45e4e64433b5b7be2812f6981000
angelilton <epifanio.rds@gmail.com>
2020-10-19 17:51:24 +0100

    add menu

---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { MdFingerprint } from 'react-icons/md'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { Button } from './Button'
+
+import './Navbar.css'
+
+const Navbar = () => {
+  const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
+
+  const handleClick = () => setClick(!click)
+
+  return (
+    <>
+      <div className='navbar'>
+        <div className='nav-barcontainer container'></div>
+
+        <Link to='/' className='navbar-logo'>
+          <MdFingerprint className='navbar-icon' />
+          LAVISH
+        </Link>
+
+        <div className='menu-icon' onClick={handleClick}>
+          {click ? <FaTimes /> : <FaBars />}
+        </div>
+
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links'>
+              Home
+            </Link>
+          </li>
+
+          <li className='nav-item'>
+            <Link to='/' className='nav-links'>
+              Services
+            </Link>
+          </li>
+
+          <li className='nav-item'>
+            <Link to='/' className='nav-links'>
+              Products
+            </Link>
+          </li>
+
+          <li class='nav-btn'>
+            {button ? (
+              <Link to='/sign-up' class='btn-link'>
+                <Button buttonStyle='btn--outline'>SIGN UP</Button>
+              </Link>
+            ) : (
+              <Link to='/sign-up' class='btn-link'>
+                <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>
+                  SIGN UP
+                </Button>
+              </Link>
+            )}
+          </li>
+
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
